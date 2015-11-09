2b03b4babb0f708832795b0159e3c8937df44632
Nicole Cordes <cordes@cps-it.de>
2015-11-09 21:58:08 +0100

    [TASK] Set dependency to TYPO3 7.6

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_company' => 'CPS-IT GmbH',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '7.4.0-7.99.99',
+			'typo3' => '7.6.0-7.99.99',
 		),
 		'conflicts' => array(
 		),
