ba79a91f7b3a58315dd4a8af40c7494ebafafb49
Nicole Cordes <cordes@cps-it.de>
2015-11-11 12:27:26 +0100

    [RELEASE] Release of multicolumn 3.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,35 +3,38 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "multicolumn".
  *
- * Auto generated 27-07-2015 20:24
+ * Auto generated 11-11-2015 12:26
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
  * writing. "version" and "dependencies" must not be touched!
  ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array(
-	'title' => 'multicolumn',
-	'description' => 'The Multicolumn extension expands TYPO3 with a new content element called Multicolumn. With the Multicolumn content element it has never been easier to do multicolumn layouts with TYPO3',
-	'category' => 'fe',
-	'version' => '3.0.0-dev',
-	'state' => 'stable',
-	'uploadfolder' => 0,
-	'createDirs' => '',
-	'clearcacheonload' => 1,
-	'author' => 'Nicole Cordes (developed by snowflake productions gmbh)',
-	'author_email' => 'cordes@cps-it.de',
-	'author_company' => 'CPS-IT GmbH',
-	'constraints' => array(
-		'depends' => array(
-			'typo3' => '7.6.0-7.99.99',
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
-	),
-	'_md5_values_when_last_written' => 'a:65:{s:9:"ChangeLog";s:4:"21b9";s:16:"ext_autoload.php";s:4:"c3f5";s:21:"ext_conf_template.txt";s:4:"e72d";s:12:"ext_icon.gif";s:4:"925c";s:17:"ext_localconf.php";s:4:"7488";s:14:"ext_tables.php";s:4:"8797";s:14:"ext_tables.sql";s:4:"ad91";s:15:"flexform_ds.xml";s:4:"ea83";s:13:"locallang.xml";s:4:"7f4e";s:26:"locallang_csh_flexform.xml";s:4:"7fd9";s:16:"locallang_db.xml";s:4:"cdde";s:12:"tsconfig.txt";s:4:"7d0c";s:26:"tt_content_multicolumn.gif";s:4:"849c";s:14:"doc/manual.sxw";s:4:"a391";s:44:"hooks/class.tx_multicolumn_alt_clickmenu.php";s:4:"94d1";s:41:"hooks/class.tx_multicolumn_cms_layout.php";s:4:"b089";s:38:"hooks/class.tx_multicolumn_db_list.php";s:4:"d9f0";s:43:"hooks/class.tx_multicolumn_t3lib_befunc.php";s:4:"282b";s:38:"hooks/class.tx_multicolumn_tcemain.php";s:4:"f597";s:50:"hooks/class.tx_multicolumn_tt_content_drawItem.php";s:4:"ee05";s:46:"hooks/class.tx_multicolumn_wizardItemsHook.php";s:4:"30f3";s:31:"lib/class.tx_multicolumn_db.php";s:4:"ce0c";s:32:"lib/class.tx_multicolumn_div.php";s:4:"731c";s:41:"lib/class.tx_multicolumn_emconfhelper.php";s:4:"351b";s:37:"lib/class.tx_multicolumn_flexform.php";s:4:"6ac6";s:36:"lib/class.tx_multicolumn_pi_base.php";s:4:"0e39";s:37:"lib/class.tx_multicolumn_tce_eval.php";s:4:"04d8";s:36:"lib/class.tx_multicolumn_tceform.php";s:4:"7c58";s:14:"pi1/ce_wiz.gif";s:4:"1863";s:32:"pi1/class.tx_multicolumn_pi1.php";s:4:"8e19";s:17:"pi1/locallang.xml";s:4:"62b9";s:24:"pi1/static/defaultTS.txt";s:4:"5bab";s:20:"pi1/static/setup.txt";s:4:"76c8";s:46:"pi_sitemap/class.tx_multicolumn_pi_sitemap.php";s:4:"c617";s:27:"pi_sitemap/static/setup.txt";s:4:"372e";s:20:"res/flashmessage.css";s:4:"22d8";s:24:"res/backend/style-v6.css";s:4:"0b0c";s:21:"res/backend/style.css";s:4:"9d02";s:25:"res/effects/locallang.xml";s:4:"0a65";s:46:"res/effects/easyAccordion/easyAccordionInit.js";s:4:"0d69";s:49:"res/effects/easyAccordion/jquery.easyAccordion.js";s:4:"efc6";s:35:"res/effects/easyAccordion/style.css";s:4:"ac20";s:43:"res/effects/roundabout/jquery.roundabout.js";s:4:"8032";s:51:"res/effects/roundabout/multicolumnImplementation.js";s:4:"fefa";s:37:"res/effects/roundabout/roundabout.css";s:4:"46c3";s:33:"res/effects/roundabout/shadow.png";s:4:"5a86";s:34:"res/effects/roundabout/sprites.png";s:4:"c9b2";s:36:"res/effects/simpleTabs/simpleTabs.js";s:4:"7f0b";s:32:"res/effects/simpleTabs/style.css";s:4:"c01e";s:47:"res/effects/sudoSlider/jquery.sudoSlider.min.js";s:4:"cc1e";s:32:"res/effects/sudoSlider/style.css";s:4:"4fc9";s:45:"res/effects/sudoSlider/sudoSliderEffectbox.js";s:4:"b54c";s:42:"res/effects/sudoSlider/images/btn_next.gif";s:4:"a1d5";s:42:"res/effects/sudoSlider/images/btn_prev.gif";s:4:"7301";s:32:"res/effects/vAccordion/style.css";s:4:"fb4f";s:36:"res/effects/vAccordion/vAccordion.js";s:4:"9a9f";s:24:"res/javascript/jQuery.js";s:4:"c80a";s:16:"res/layout/1.gif";s:4:"e38a";s:17:"res/layout/10.gif";s:4:"7286";s:16:"res/layout/2.gif";s:4:"c1a4";s:16:"res/layout/3.gif";s:4:"5f77";s:27:"res/layout/effectSlider.gif";s:4:"c982";s:24:"res/layout/locallang.xml";s:4:"f2be";s:45:"res/layout/makeEqualElementBoxColumnHeight.js";s:4:"ac80";s:39:"res/layout/makeEqualElementBoxHeight.js";s:4:"0aff";}',
+$EM_CONF[$_EXTKEY] = array (
+  'title' => 'multicolumn',
+  'description' => 'The Multicolumn extension expands TYPO3 with a new content element called Multicolumn. With the Multicolumn content element it has never been easier to do multicolumn layouts with TYPO3',
+  'category' => 'fe',
+  'version' => '3.0.0',
+  'state' => 'stable',
+  'uploadfolder' => 0,
+  'createDirs' => '',
+  'clearcacheonload' => 1,
+  'author' => 'Nicole Cordes (developed by snowflake productions gmbh)',
+  'author_email' => 'cordes@cps-it.de',
+  'author_company' => 'CPS-IT GmbH',
+  'constraints' => 
+  array (
+    'depends' => 
+    array (
+      'typo3' => '7.6.0-7.99.99',
+    ),
+    'conflicts' => 
+    array (
+    ),
+    'suggests' => 
+    array (
+    ),
+  ),
+  '_md5_values_when_last_written' => 'a:64:{s:9:"ChangeLog";s:4:"b947";s:21:"ext_conf_template.txt";s:4:"e72d";s:12:"ext_icon.gif";s:4:"925c";s:17:"ext_localconf.php";s:4:"9632";s:14:"ext_tables.php";s:4:"7a8f";s:14:"ext_tables.sql";s:4:"ad91";s:15:"flexform_ds.xml";s:4:"ea83";s:13:"locallang.xml";s:4:"7f4e";s:26:"locallang_csh_flexform.xml";s:4:"7fd9";s:16:"locallang_db.xml";s:4:"cdde";s:12:"tsconfig.txt";s:4:"7d0c";s:26:"tt_content_multicolumn.gif";s:4:"849c";s:42:"Configuration/TCA/Overrides/tt_content.php";s:4:"26c0";s:14:"doc/manual.sxw";s:4:"a391";s:44:"hooks/class.tx_multicolumn_alt_clickmenu.php";s:4:"0351";s:41:"hooks/class.tx_multicolumn_cms_layout.php";s:4:"b089";s:38:"hooks/class.tx_multicolumn_db_list.php";s:4:"d9f0";s:43:"hooks/class.tx_multicolumn_t3lib_befunc.php";s:4:"f58c";s:38:"hooks/class.tx_multicolumn_tcemain.php";s:4:"86bc";s:50:"hooks/class.tx_multicolumn_tt_content_drawItem.php";s:4:"18ce";s:46:"hooks/class.tx_multicolumn_wizardItemsHook.php";s:4:"fdc4";s:31:"lib/class.tx_multicolumn_db.php";s:4:"7e45";s:32:"lib/class.tx_multicolumn_div.php";s:4:"8c29";s:41:"lib/class.tx_multicolumn_emconfhelper.php";s:4:"f37f";s:37:"lib/class.tx_multicolumn_flexform.php";s:4:"6073";s:36:"lib/class.tx_multicolumn_pi_base.php";s:4:"4640";s:37:"lib/class.tx_multicolumn_tce_eval.php";s:4:"1bb4";s:36:"lib/class.tx_multicolumn_tceform.php";s:4:"e127";s:14:"pi1/ce_wiz.gif";s:4:"1863";s:32:"pi1/class.tx_multicolumn_pi1.php";s:4:"679f";s:17:"pi1/locallang.xml";s:4:"62b9";s:24:"pi1/static/defaultTS.txt";s:4:"5bab";s:20:"pi1/static/setup.txt";s:4:"76c8";s:46:"pi_sitemap/class.tx_multicolumn_pi_sitemap.php";s:4:"7db3";s:27:"pi_sitemap/static/setup.txt";s:4:"542c";s:20:"res/flashmessage.css";s:4:"22d8";s:21:"res/backend/style.css";s:4:"0b0c";s:25:"res/effects/locallang.xml";s:4:"0a65";s:46:"res/effects/easyAccordion/easyAccordionInit.js";s:4:"0d69";s:49:"res/effects/easyAccordion/jquery.easyAccordion.js";s:4:"efc6";s:35:"res/effects/easyAccordion/style.css";s:4:"ac20";s:43:"res/effects/roundabout/jquery.roundabout.js";s:4:"8032";s:51:"res/effects/roundabout/multicolumnImplementation.js";s:4:"fefa";s:37:"res/effects/roundabout/roundabout.css";s:4:"46c3";s:33:"res/effects/roundabout/shadow.png";s:4:"5a86";s:34:"res/effects/roundabout/sprites.png";s:4:"c9b2";s:36:"res/effects/simpleTabs/simpleTabs.js";s:4:"7f0b";s:32:"res/effects/simpleTabs/style.css";s:4:"c01e";s:47:"res/effects/sudoSlider/jquery.sudoSlider.min.js";s:4:"cc1e";s:32:"res/effects/sudoSlider/style.css";s:4:"4fc9";s:45:"res/effects/sudoSlider/sudoSliderEffectbox.js";s:4:"b54c";s:42:"res/effects/sudoSlider/images/btn_next.gif";s:4:"a1d5";s:42:"res/effects/sudoSlider/images/btn_prev.gif";s:4:"7301";s:32:"res/effects/vAccordion/style.css";s:4:"fb4f";s:36:"res/effects/vAccordion/vAccordion.js";s:4:"9a9f";s:24:"res/javascript/jQuery.js";s:4:"c80a";s:16:"res/layout/1.gif";s:4:"e38a";s:17:"res/layout/10.gif";s:4:"7286";s:16:"res/layout/2.gif";s:4:"c1a4";s:16:"res/layout/3.gif";s:4:"5f77";s:27:"res/layout/effectSlider.gif";s:4:"c982";s:24:"res/layout/locallang.xml";s:4:"f2be";s:45:"res/layout/makeEqualElementBoxColumnHeight.js";s:4:"ac80";s:39:"res/layout/makeEqualElementBoxHeight.js";s:4:"0aff";}',
 );
 
-?>
