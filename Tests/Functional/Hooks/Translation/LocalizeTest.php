e1a79f28402dee6f474c83b193021928071ffec6
Nicole Cordes <cordes@cps-it.de>
2016-06-30 12:27:50 +0200

    [TASK] Add functional test for localization of nested containers

--- Tests/Functional/Hooks/Translation/LocalizeTest.php
+++ Tests/Functional/Hooks/Translation/LocalizeTest.php
@@ -77,4 +77,84 @@ class tx_multicolumn_tcemainTranslationLocalizeTest extends tx_multicolumn_tcema
         );
         $this->assertSame(1, $count);
     }
+
+    /**
+     * Localize a nested multicolumn container from default language
+     *
+     * @test
+     */
+    public function localizeNestedContainersAndChildrenFromDefaultLanguage()
+    {
+        $fixturePath = ORIGINAL_ROOT . 'typo3conf/ext/multicolumn/Tests/Functional/Fixtures/';
+        $this->importDataSet($fixturePath . 'tt_content_nested_container.xml');
+
+        $cmdMap = [
+            tx_multicolumn_tcemainBaseTest::CONTENT_TABLE => [
+                1 => [
+                    'localize' => 1,
+                ],
+            ],
+        ];
+
+        $dataHandler = new DataHandler();
+        $dataHandler->start([], $cmdMap);
+        $dataHandler->process_cmdmap();
+
+        $containerUid = $dataHandler->copyMappingArray[tx_multicolumn_tcemainBaseTest::CONTENT_TABLE][1];
+        $childUid = $dataHandler->copyMappingArray[tx_multicolumn_tcemainBaseTest::CONTENT_TABLE][2];
+        $nestedContainerUid = $dataHandler->copyMappingArray[tx_multicolumn_tcemainBaseTest::CONTENT_TABLE][4];
+        $nestedChildUid = $dataHandler->copyMappingArray[tx_multicolumn_tcemainBaseTest::CONTENT_TABLE][5];
+
+        $count = $this->getDatabaseConnection()->exec_SELECTcountRows(
+            '*',
+            tx_multicolumn_tcemainBaseTest::CONTENT_TABLE,
+            'uid=' . $containerUid
+            . ' AND pid=1'
+            . ' AND deleted=0'
+            . ' AND CType=\'' . tx_multicolumn_tcemainBaseTest::CTYPE_MULTICOLUMN . '\''
+            . ' AND colPos=0'
+            . ' AND sys_language_uid=1'
+            . ' AND tx_multicolumn_parentid=0'
+        );
+        $this->assertSame(1, $count, 'Container was not found');
+
+        $count = $this->getDatabaseConnection()->exec_SELECTcountRows(
+            '*',
+            tx_multicolumn_tcemainBaseTest::CONTENT_TABLE,
+            'uid=' . $childUid
+            . ' AND pid=1'
+            . ' AND deleted=0'
+            . ' AND CType=\'' . tx_multicolumn_tcemainBaseTest::CTYPE_TEXTPIC . '\''
+            . ' AND colPos=10'
+            . ' AND sys_language_uid=1'
+            . ' AND tx_multicolumn_parentid=' . $containerUid
+        );
+        $this->assertSame(1, $count, 'Child was not found');
+
+        $count = $this->getDatabaseConnection()->exec_SELECTcountRows(
+            '*',
+            tx_multicolumn_tcemainBaseTest::CONTENT_TABLE,
+            'uid=' . $nestedContainerUid
+            . ' AND pid=1'
+            . ' AND deleted=0'
+            . ' AND CType=\'' . tx_multicolumn_tcemainBaseTest::CTYPE_MULTICOLUMN . '\''
+            . ' AND colPos=10'
+            . ' AND sys_language_uid=1'
+            . ' AND tx_multicolumn_parentid=' . $containerUid
+        );
+        $this->assertSame(1, $count, 'Nested container was not found');
+
+        $count = $this->getDatabaseConnection()->exec_SELECTcountRows(
+            '*',
+            tx_multicolumn_tcemainBaseTest::CONTENT_TABLE,
+            'uid=' . $nestedChildUid
+            . ' AND pid=1'
+            . ' AND deleted=0'
+            . ' AND CType=\'' . tx_multicolumn_tcemainBaseTest::CTYPE_TEXTPIC . '\''
+            . ' AND colPos=10'
+            . ' AND sys_language_uid=1'
+            . ' AND tx_multicolumn_parentid=' . $nestedContainerUid
+        );
+        $this->assertSame(1, $count, 'Nested container child was not found');
+    }
 }
