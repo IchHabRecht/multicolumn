fa18bfbf3e70b1736519eedf2b79e759c19dd498
Nicole Cordes <typo3@cordes.co>
2017-04-18 19:20:38 +0200

    [TASK] Modify PasteTest to respect new ContextMenu API

--- Tests/Functional/Hooks/DefaultLanguage/PasteTest.php
+++ Tests/Functional/Hooks/DefaultLanguage/PasteTest.php
@@ -27,6 +27,7 @@ require_once __DIR__ . '/../../../FunctionalBaseTest.php';
 
 use TYPO3\CMS\Backend\ClickMenu\ClickMenu;
 use TYPO3\CMS\Backend\Clipboard\Clipboard;
+use TYPO3\CMS\Backend\ContextMenu\ContextMenu;
 use TYPO3\CMS\Backend\Utility\BackendUtility;
 use TYPO3\CMS\Core\DataHandling\DataHandler;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
@@ -43,28 +44,74 @@ class tx_multicolumn_alt_clickmenuTest extends tx_multicolumn_tcemainBaseTest
         $table = tx_multicolumn_tcemainBaseTest::CONTENT_TABLE;
         $uid = 10;
 
-        $clipObj = new Clipboard();
-        $clipObj->clipData = [
-            'current' => 'normal',
-            'normal' => [
-                'el' => [
-                    $table . '|3' => 1,
+        if (!class_exists(ClickMenu::class)) {
+            $expectedSubset = [
+                'multicolumn-pasteinto-0' => [
+                    'additionalAttributes' => [
+                        'data-callback-module' => 'TYPO3/CMS/Multicolumn/ContextMenuActions',
+                        'data-colpos' => 10,
+                    ],
+                    'callbackAction' => 'pasteIntoColumn',
+                    'label' => 'Paste into column 1',
+                    'type' => 'item',
                 ],
-            ],
-        ];
-        $clipObj->lockToNormal();
+                'multicolumn-pasteinto-1' => [
+                    'additionalAttributes' => [
+                        'data-callback-module' => 'TYPO3/CMS/Multicolumn/ContextMenuActions',
+                        'data-colpos' => 11,
+                    ],
+                    'callbackAction' => 'pasteIntoColumn',
+                    'label' => 'Paste into column 2',
+                    'type' => 'item',
+                ],
+                'multicolumn-pasteinto-2' => [
+                    'additionalAttributes' => [
+                        'data-callback-module' => 'TYPO3/CMS/Multicolumn/ContextMenuActions',
+                        'data-colpos' => 12,
+                    ],
+                    'callbackAction' => 'pasteIntoColumn',
+                    'label' => 'Paste into column 3',
+                    'type' => 'item',
+                ],
+            ];
 
-        $clickMenu = new ClickMenu();
-        $clickMenu->clipObj = $clipObj;
-        $clickMenu->extClassArray = $GLOBALS['TBE_MODULES_EXT']['xMOD_alt_clickmenu']['extendCMclasses'];
-        $clickMenu->rec = BackendUtility::getRecordWSOL($table, $uid);
-        $clickMenuContent = $clickMenu->printDBClickMenu($table, $uid);
+            $GLOBALS['BE_USER']->pushModuleData(
+                'clipboard',
+                [
+                    'normal' => [
+                        'el' => [
+                            $table . '|3' => 1,
+                        ],
+                    ],
+                ]
+            );
+            $contextMenu = new ContextMenu();
 
-        $pasteUrl = $clipObj->pasteUrl('tt_content', -$uid, 0);
+            $this->assertArraySubset($expectedSubset, $contextMenu->getItems($table, $uid, ''));
+        } else {
+            $clipboardObject = new Clipboard();
+            $clipboardObject->clipData = [
+                'current' => 'normal',
+                'normal' => [
+                    'el' => [
+                        $table . '|3' => 1,
+                    ],
+                ],
+            ];
+            $clipboardObject->lockToNormal();
 
-        $this->assertContains(trim(GeneralUtility::quoteJSvalue($pasteUrl . '&tx_multicolumn[action]=pasteInto&colPos=10&tx_multicolumn_parentid=1'), '\''), $clickMenuContent);
-        $this->assertContains(trim(GeneralUtility::quoteJSvalue($pasteUrl . '&tx_multicolumn[action]=pasteInto&colPos=11&tx_multicolumn_parentid=1'), '\''), $clickMenuContent);
-        $this->assertContains(trim(GeneralUtility::quoteJSvalue($pasteUrl . '&tx_multicolumn[action]=pasteInto&colPos=12&tx_multicolumn_parentid=1'), '\''), $clickMenuContent);
+            $clickMenu = new ClickMenu();
+            $clickMenu->clipObj = $clipboardObject;
+            $clickMenu->extClassArray = $GLOBALS['TBE_MODULES_EXT']['xMOD_alt_clickmenu']['extendCMclasses'];
+            $clickMenu->rec = BackendUtility::getRecordWSOL($table, $uid);
+            $clickMenuContent = $clickMenu->printDBClickMenu($table, $uid);
+
+            $pasteUrl = $clipboardObject->pasteUrl('tt_content', -$uid, 0);
+
+            $this->assertContains(trim(GeneralUtility::quoteJSvalue($pasteUrl . '&tx_multicolumn[action]=pasteInto&colPos=10&tx_multicolumn_parentid=1'), '\''), $clickMenuContent);
+            $this->assertContains(trim(GeneralUtility::quoteJSvalue($pasteUrl . '&tx_multicolumn[action]=pasteInto&colPos=11&tx_multicolumn_parentid=1'), '\''), $clickMenuContent);
+            $this->assertContains(trim(GeneralUtility::quoteJSvalue($pasteUrl . '&tx_multicolumn[action]=pasteInto&colPos=12&tx_multicolumn_parentid=1'), '\''), $clickMenuContent);
+        }
     }
 
     /**
