dff0cc2971c3f20692c40cba5f70ed11eb57f253
Kevin Purrmann <kpurrmann@Kevins-MBP.fritz.box>
2017-08-07 10:12:21 +0200

    [BUGFIX] Fix imports

--- lib/class.tx_multicolumn_db.php
+++ lib/class.tx_multicolumn_db.php
@@ -26,6 +26,7 @@
 use TYPO3\CMS\Backend\View\PageLayoutView;
 use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Database\Query\Restriction\BackendWorkspaceRestriction;
+use TYPO3\CMS\Core\Database\Query\Restriction\HiddenRestriction;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class tx_multicolumn_db
