359359958609264978e3d4ea256501e038222c08
Nicole Cordes <typo3@cordes.co>
2017-08-08 12:55:19 +0200

    [TASK] Add and streamline header comments

--- Classes/ContextMenu/ItemProvider.php
+++ Classes/ContextMenu/ItemProvider.php
@@ -1,6 +1,17 @@
 <?php
 namespace IchHabRecht\Multicolumn\ContextMenu;
 
+/*
+ * This file is part of the TYPO3 Multicolumn project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read
+ * LICENSE file that was distributed with this source code.
+ */
+
 use TYPO3\CMS\Backend\ContextMenu\ItemProviders\RecordProvider;
 use TYPO3\CMS\Lang\LanguageService;
 
